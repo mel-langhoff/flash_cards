7b6900edccba7eef4f7e82ddae987376df9d419e
Mel Langhoff <mlanghoff@uwalumni.com>
2023-11-30 21:46:54 -0700

    Add test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -63,17 +63,17 @@ RSpec.describe Round do
         expect(new_turn.class).to eq (Turn)
     end
 
-    # it "#correct?" do
-    #     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    #     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    #     cards = [card_1, card_2, card_3]
-    #     deck = Deck.new(cards)
-    #     round = Round.new(deck)
-    #     new_turn = round.take_turn("Juneau")
+    it "#correct?" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
 
-    #     expect(new_turn.correct?).to eq(true)
-    # end
+        expect(new_turn.correct?).to eq(true)
+    end
 
     it "#number_correct" do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -144,7 +144,4 @@ RSpec.describe Round do
         expect(round.percent_correct_by_category(:Geography)).to eq(100)
         expect(round.current_card).to eq(card_3)
     end
-
-
-
 end
