abe43eae253a3baa1fc4fcafddf010f79a5c2577
Mel Langhoff <mlanghoff@uwalumni.com>
2023-12-04 09:56:54 -0700

    Refine tests

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,15 +4,15 @@ require "./lib/card"
 require "./lib/round"
 require "./lib/card_generator"
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-card_4 = Card.new("What is 5 + 5?", "10", :Math)
-cards = [card_1, card_2, card_3, card_4]
-deck = Deck.new(cards)
-# filename = "cards.txt"
-# cards = CardGenerator.new(filename)
-# deck = Deck.new(cards.cards_array)
+# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# card_4 = Card.new("What is 5 + 5?", "10", :Math)
+# cards = [card_1, card_2, card_3, card_4]
+# deck = Deck.new(cards)
+filename = "cards.txt"
+cards = CardGenerator.new(filename)
+deck = Deck.new(cards.cards_array)
 round = Round.new(deck)
 
 round.start
